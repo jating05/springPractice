949e04e8f2cc6785e99d392ec8ee004da01bf2e0
jatingoyal <jatin.goyal@exotel.com>
2024-07-05 12:24:27 +0530

    Spring Practice

---
+++ src/main/java/com/masti/jatin/ShontyBhaiApplication.java
@@ -0,0 +1,13 @@
+package com.masti.jatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShontyBhaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShontyBhaiApplication.class, args);
+	}
+
+}
